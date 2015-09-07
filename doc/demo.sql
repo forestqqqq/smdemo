e0da5b48e809e6fc20e05039f19c8ef527735349
张海威 <forestqqqq@163.com>
2015-09-07 15:16:03 +0800

    添加最新的数据库

--- doc/demo.sql
+++ doc/demo.sql
@@ -15,6 +15,21 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`demo` /*!40100 DEFAULT CHARACTER SET ut
 
 USE `demo`;
 
+/*Table structure for table `authority` */
+
+DROP TABLE IF EXISTS `authority`;
+
+CREATE TABLE `authority` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `username` varchar(100) DEFAULT NULL COMMENT '用户名',
+  `role` varchar(100) DEFAULT NULL COMMENT '角色名',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC COMMENT='权限角色表';
+
+/*Data for the table `authority` */
+
+insert  into `authority`(`id`,`username`,`role`) values (1,'forest','ROLE_ADMIN');
+
 /*Table structure for table `user` */
 
 DROP TABLE IF EXISTS `user`;
@@ -22,13 +37,14 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `username` varchar(100) DEFAULT NULL COMMENT '用户名',
-  `name` varchar(100) DEFAULT NULL COMMENT '密码',
+  `password` varchar(40) DEFAULT NULL COMMENT '密码',
+  `name` varchar(100) DEFAULT NULL COMMENT '姓名',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 /*Data for the table `user` */
 
-insert  into `user`(`id`,`username`,`name`) values (1,'forest','aaa');
+insert  into `user`(`id`,`username`,`password`,`name`) values (1,'forest','f379cfd7a55b621577a8389d1817a102','张海威');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
